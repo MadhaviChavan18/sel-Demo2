226d3e3e66fc6ce01fe1771089b2f01e33ce52a5
desir <satish.pawale687@gmail.com>
2023-10-18 19:00:10 +0530

    third.py deleted 10 lines of code By Satish Pawale

--- Third.py
+++ Third.py
@@ -5,15 +5,3 @@ str = input()
 
 strRev = str[::-1]
 str = strRev
-print("\nReverse")
-print("\nReverse")
-print("\nReverse")
-print("\nReverse")
-print("\nReverse")
-print("\nReverse")
-print("\nReverse")
-print("\nReverse")
-print("\nReverse")
-print("\nReverse")
-print("\nReverse")
-print("\nReverse")
